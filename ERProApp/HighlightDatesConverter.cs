affbce7e9ff21c3d44a7dfa508a1fb088b402862
Jonas Posselt <jonas.posselt@web.de>
2017-05-28 15:19:28 +0200

    Highlight dates in calendar

---
+++ ERProApp/HighlightDatesConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ERProApp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    //[ValueConversion(typeof(), typeof())]
+    class HighlightDatesConverter : IValueConverter
+    {
+        private static Dictionary<DateTime, string> _highlight = new Dictionary<DateTime, string>();
+
+        /// <summary>
+        /// Gibt für ein Datum einen Textstring aus einem Dictionary zurueck. Falls ein Datum keinen
+        /// Schluessel im Dictionary hat ist der Rueckgabewert null;
+        /// </summary>
+        public static Dictionary<DateTime, string> Highlight
+        {
+            get { return _highlight; }
+            set { _highlight = value; }
+        }
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text;
+            if (value == null)
+                text = null;
+            else if (!_highlight.TryGetValue((DateTime)value, out text))
+                text = null;
+            return text;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+}
--- ERProApp/MainWindow.xaml.cs
+++ ERProApp/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using Microsoft.Windows.Controls.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
-
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ERProApp
 {
@@ -25,5 +29,44 @@ namespace ERProApp
             var result = ExitMessageBox.ShowDialog();
             if (result == false) { e.Cancel = true; }
         }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            if (e.AddedItems != null && e.AddedItems.Count > 0)
+            {
+                // Ausgewaehltes Element
+                Rental selected = dataGrid.SelectedItem as Rental;
+
+                // Kein Element ausgewaehlt
+                if (selected == null)
+                {
+                    e.Handled = true;
+                    return;
+                }
+                else
+                {
+                    Dictionary<DateTime, string> highlight = new Dictionary<DateTime, string>();
+
+                    // Iteriere ueber Tage der Ausleihe
+                    for(var day = selected.StartDate.Date; day.Date <= selected.EndDate.Date; day = day.AddDays(1))
+                    {
+                        highlight.Add(day,"Dummy");
+                    }
+                    HighlightDatesConverter.Highlight = highlight;
+
+                    // Workaround zur Aktualisierung der Kalenderansicht,
+                    // da die Invalidate Methoden hier nicht wie erwartet
+                    // funktioniern
+                    RentalCalendar.DisplayDate = DateTime.MinValue;
+                    RentalCalendar.DisplayDate = selected.StartDate;
+
+                    e.Handled = true;
+                    return;
+                }
+                
+            }
+        }
+
     }
 }
