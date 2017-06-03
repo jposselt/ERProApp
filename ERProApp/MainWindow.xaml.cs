e9821ef316147fc67fdcaab50570cafaaf57dd5c
Jonas Posselt <jonas.posselt@web.de>
2017-06-03 19:47:57 +0200

    Added accelerator keys

--- ERProApp/MainWindow.xaml.cs
+++ ERProApp/MainWindow.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-
+using System.Windows.Input;
 
 namespace ERProApp
 {
@@ -74,7 +74,7 @@ namespace ERProApp
         }
 
         // Eventhandler zum Loeschen der ausgewaelten Ausleihe/Reservierung
-        private void dataGrid_Delete(object sender, RoutedEventArgs e)
+        /*private void dataGrid_Delete(object sender, RoutedEventArgs e)
         {
             if (RentalData.SelectedItem == null)
             {
@@ -114,10 +114,10 @@ namespace ERProApp
                     RentalData.InvalidateVisual();
                 }
             }
-        }
+        }*/
 
         // Eventhandler zum Umwandeln der ausgewaelten Reservierung in eine Ausleihe
-        private void dataGrid_ReservationToRent(object sender, RoutedEventArgs e)
+        /*private void dataGrid_ReservationToRent(object sender, RoutedEventArgs e)
         {
             if (RentalData.SelectedItem != null)
             {
@@ -133,9 +133,9 @@ namespace ERProApp
                 }
 
                 // Aktualisiere Datagrid
-                RentalData.InvalidateVisual();
+                _vc.Rentals.Refresh();
             }
-        }
+        }*/
 
         // Eventhandler zum Anzeigen der detailierten Gegenstandsinformationen
         private void dataGrid_ShowItemInfo(object sender, RoutedEventArgs e)
@@ -157,7 +157,7 @@ namespace ERProApp
         }
 
         // Eventhandler zum sperren eines Buchs
-        private void dataGrid_BlockItem(object sender, RoutedEventArgs e)
+        /*private void dataGrid_BlockItem(object sender, RoutedEventArgs e)
         {
             if (RentalData.SelectedItem != null)
             {
@@ -170,10 +170,10 @@ namespace ERProApp
                 // Aktualisiere Datagrid
                 RentalData.InvalidateVisual();
             }
-        }
+        }*/
 
         // Eventhandler zum entsperren eines Buchs
-        private void dataGrid_UnblockItem(object sender, RoutedEventArgs e)
+        /*private void dataGrid_UnblockItem(object sender, RoutedEventArgs e)
         {
             if (RentalData.SelectedItem != null)
             {
@@ -201,7 +201,7 @@ namespace ERProApp
                 // Aktualisiere Datagrid
                 RentalData.InvalidateVisual();
             }
-        }
+        }*/
 
         // Eventhandler zum Suche nach Kunden
         private void button_SearchCustomer(object sender, RoutedEventArgs e)
@@ -238,13 +238,263 @@ namespace ERProApp
         }
 
         // Eventhandler zum des About Fensters
-        private void button_ShowAbout(object sender, RoutedEventArgs e)
+        /*private void button_ShowAbout(object sender, RoutedEventArgs e)
         {
             AboutView about = new AboutView();
             about.ShowDialog();
-        }
+        }*/
 
         #endregion // Eventhandlers
+
+        #region Commands
+
+        /// <summary>
+        /// Anzeigen der detailierten Gegenstandsinformationen
+        /// </summary>
+        public static readonly RoutedCommand ItemInfo = new RoutedCommand();
+
+        private void ItemInfoCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (RentalData != null && RentalData.SelectedItem != null)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+            e.Handled = true;
+        }
+
+        private void ItemInfoExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            ItemInfoView itemInfo = new ItemInfoView();
+            itemInfo.Owner = this;
+            itemInfo.DataContext = this.RentalData.SelectedItem;
+            itemInfo.ShowDialog();
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Anzeigen der detailierten Kundeninformationen
+        /// </summary>
+        public static readonly RoutedCommand CustomerInfo = new RoutedCommand();
+
+        private void CustomerInfoCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (RentalData != null && RentalData.SelectedItem != null)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+            e.Handled = true;
+        }
+
+        private void CustomerInfoExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            CustomerInfoView customerInfo = new CustomerInfoView();
+            customerInfo.Owner = this;
+            customerInfo.Rentals = new CollectionViewSource() { Source = _vc.Rentals.SourceCollection };
+            customerInfo.SelectedCustomer = ((Rental)this.RentalData.SelectedItem).Taker;
+            customerInfo.ShowDialog();
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Loeschen von Ausleihen und Reservierungen
+        /// </summary>
+        public static readonly RoutedCommand Delete = new RoutedCommand();
+
+        private void DeleteCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (RentalData != null && RentalData.SelectedItem != null)
+                e.CanExecute = true;
+            else
+                e.CanExecute = false;
+            e.Handled = true;
+        }
+
+        private void DeleteExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (RentalData.SelectedItem == null)
+            {
+                MessageBox.Show("Es wurde keine Ausleihe/Reservierung ausgewählt", "Keine Auswahl", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                if ((ViewController.DeleteImmediately) || (DeleteMessageBox.ShowDialog(this) == true))
+                {
+                    // Update des Buchstatus und des Reservierungszaehlers
+                    if ((RentalData.SelectedItem as Rental).Reservation)
+                    {
+                        Book temp = (RentalData.SelectedItem as Rental).Item;
+                        temp.ReservationCount -= 1;
+                        if (temp.Status == "reserviert" && temp.ReservationCount == 0)
+                        {
+                            temp.Status = "verfügbar";
+                        }
+                    }
+                    else
+                    {
+                        Book temp = (RentalData.SelectedItem as Rental).Item;
+                        if (temp.Status == "ausgeliehen" && temp.ReservationCount == 0)
+                        {
+                            temp.Status = "verfügbar";
+                        }
+                        else
+                        {
+                            temp.Status = "reserviert";
+                        }
+                    }
+
+                    // Lösche die Ausleihe
+                    DataController.DeleteRental(RentalData.SelectedItem as Rental);
+
+                    // Aktualisiere Datagrid
+                    _vc.Rentals.Refresh();
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Zeigt das "About" Fenster an
+        /// </summary>
+        public static readonly RoutedCommand About = new RoutedCommand();
+
+        private void AboutCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+
+        private void AboutExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            AboutView about = new AboutView();
+            about.ShowDialog();
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Umwandeln einer Reservierung in Ausleihe
+        /// </summary>
+        public static readonly RoutedCommand Checkout = new RoutedCommand();
+
+        private void CheckoutCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (RentalData != null && RentalData.SelectedItem != null)
+            {
+                if( (RentalData.SelectedItem as Rental).Reservation  && !(RentalData.SelectedItem as Rental).Item.Blocked)
+                {
+                    e.CanExecute = true;
+                }
+                else
+                    e.CanExecute = false;
+            }
+            else
+                e.CanExecute = false;
+            e.Handled = true;
+        }
+
+        private void CheckoutExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (RentalData.SelectedItem != null)
+            {
+                // Reservation Feld setzen
+                (RentalData.SelectedItem as Rental).Reservation = false;
+
+                // Update des Buchstatus und des Reservierungszaehlers
+                Book temp = (RentalData.SelectedItem as Rental).Item;
+                temp.ReservationCount -= 1;
+                if (temp.Status == "reserviert" && temp.ReservationCount == 0)
+                {
+                    temp.Status = "verfügbar";
+                }
+
+                // Aktualisiere Datagrid
+                _vc.Rentals.Refresh();
+
+            }
+            e.Handled = true;
+        }
+
+
+        /// <summary>
+        /// Sperrt Ausleihen des ausgewaehlten Buchs
+        /// </summary>
+        public static readonly RoutedCommand Block = new RoutedCommand();
+
+        private void BlockCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (RentalData != null && RentalData.SelectedItem != null)
+            {
+                if ((RentalData.SelectedItem as Rental).Item.Blocked)
+                    e.CanExecute = false;
+                else
+                    e.CanExecute = true;
+            }
+            else
+                e.CanExecute = false;
+            
+            e.Handled = true;
+        }
+
+        private void BlockExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (RentalData.SelectedItem != null)
+            {
+                // Setze Feld
+                (RentalData.SelectedItem as Rental).Item.Blocked = true;
+
+                // Update Status
+                (RentalData.SelectedItem as Rental).Item.Status = "gesperrt";
+            }
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Entsperrt Ausleihen des ausgewaehlten Buchs
+        /// </summary>
+        public static readonly RoutedCommand Unblock = new RoutedCommand();
+
+        private void UnblockCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (RentalData != null && RentalData.SelectedItem != null)
+            {
+                if ((RentalData.SelectedItem as Rental).Item.Blocked)
+                    e.CanExecute = true;
+                else
+                    e.CanExecute = false;
+            }
+            else
+                e.CanExecute = false;
+            
+            e.Handled = true;
+        }
+
+        private void UnblockExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Setze Feld
+            (RentalData.SelectedItem as Rental).Item.Blocked = false;
+
+            // Update Status
+            string id = (RentalData.SelectedItem as Rental).ItemID;
+            (RentalData.SelectedItem as Rental).Item.Status = "verfügbar";
+            foreach (DataGridRow dr in RentalData.Items)
+            {
+                if ((dr.Item as Rental).ItemID == id)
+                {
+                    if ((RentalData.SelectedItem as Rental).Item.Status == "verfügbar" && (RentalData.SelectedItem as Rental).Reservation)
+                    {
+                        (RentalData.SelectedItem as Rental).Item.Status = "reserviert";
+                    }
+                    else
+                    {
+                        (RentalData.SelectedItem as Rental).Item.Status = "ausgeliehen";
+                    }
+                }
+            }
+            e.Handled = true;
+        }
+
+        #endregion // Commands
+
 
     }
 }
